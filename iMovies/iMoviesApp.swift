a33c369b550ae84466ea2cf27224b3e493f2b173
Inan Mahmud <inan.mahmud@cefalo.com>
2023-12-24 23:20:04 +0600

    Initial Commit

---
+++ iMovies/iMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMoviesApp.swift
+//  iMovies
+//
+//  Created by cefalo on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
