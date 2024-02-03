8e502654c3390744a19802068416455388c5e803
Inan Mahmud <inan.mahmud@cefalo.com>
2024-02-03 14:30:22 +0600

    feat: add user struct

---
+++ iMovies/Feature/Dashboard/Presentation/Account/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  iMovies
+//
+//  Created by cefalo on 3/2/24.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullName: String
+    let email: String
+    
+    var initials: String {
+        let formatter = PersonNameComponentsFormatter()
+        if let components = formatter.personNameComponents(from: fullName) {
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        return ""
+    }
+}
