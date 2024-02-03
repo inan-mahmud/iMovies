4a30f6385a4e8e94f1de2c07efd8701aa9bb0565
Inan Mahmud <inan.mahmud@cefalo.com>
2024-02-03 11:27:49 +0600

    feat: add input view component

--- iMovies/Feature/Login/LoginView.swift
+++ iMovies/Feature/Login/LoginView.swift
@@ -9,11 +9,39 @@ import SwiftUI
 
 struct LoginView: View {
     
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         ZStack {
             Color(.systemBackground)
             
-            
+            VStack {
+                
+                VStack (spacing: 24){
+                    InputView(text: $email, title: "Email Address", placeholder: "name@example.com")
+                        .textInputAutocapitalization(.none)
+                    
+                    InputView(text: $password, title: "Password", placeholder: "Enter your password",isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top,12)
+                
+                Button {
+                    print("Log user in ...")
+                } label: {
+                    HStack {
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName:"arrow.right")
+                    }.foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top,24)
+                
+            }
         }
     }
 }
